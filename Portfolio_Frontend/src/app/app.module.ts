d647040540a9e79a5bf3dc63a381e3432800da49
Jesica <jesicamosse@gmail.com>
2022-12-05 10:56:11 -0300

    Ultimos cambios

--- Portfolio_Frontend/src/app/app.module.ts
+++ Portfolio_Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
