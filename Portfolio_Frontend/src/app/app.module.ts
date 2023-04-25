8d5d0e84f7412c1f9ed52ac384ed9bc50fc454c8
Jesica <jesicamosse@gmail.com>
2023-04-25 12:11:27 -0300

    ultimos cambios

--- Portfolio_Frontend/src/app/app.module.ts
+++ Portfolio_Frontend/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardysoftComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
